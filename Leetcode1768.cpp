f3fd08ac81eae4cf3818efd52b27347fc69ec92c
kavindar <kavindarofficially@gmail.com>
2025-02-27 09:43:03 +0530

    Leetcode1768

---
+++ Leetcode1768.cpp
@@ -0,0 +1,59 @@
+/*
+1768. Merge Strings Alternately
+Solved
+Easy
+Topics
+Companies
+Hint
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+ 
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters.
+*/
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n1=word1.length(),n2=word2.length();
+        int i=0;
+        string res="";
+        while(i<n1||i<n2){
+            if(i<n1) res+=word1.at(i);
+            if(i<n2) res+=word2.at(i);
+            i++;
+        }
+        return res;
+    }
+};
